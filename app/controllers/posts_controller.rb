613c34a274fd45aca4fb25692ff6445b77e4f514
Alex Lee <hyunho98@gmail.com>
2020-05-04 10:24:26 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def update
     set_post!
 
-    if @post.update(post_params)?
+    if @post.update(post_params).valid?
       @post.update(post_params)
 
       redirect_to post_path(@post)
